b7c1db8d9b4b9f6db3361c94f5c4534f0276b955
Maria Rodrigues <mlnr@cin.ufpe.br>
2018-06-29 15:25:35 -0300

    Modificacoes na MacroManager

--- project/MacroManager.h
+++ project/MacroManager.h
@@ -1,8 +1,20 @@
 #pragma once
 
 #include <sc2api/sc2_api.h>
+#include <sc2api/sc2_interfaces.h>
 
 class ZergBot;
+
+struct IsTownHall {
+	bool operator()(const sc2::Unit& unit) {
+		switch (unit.unit_type.ToType()) {
+		case sc2::UNIT_TYPEID::ZERG_HATCHERY: return true;
+		case sc2::UNIT_TYPEID::ZERG_LAIR: return true;
+		case sc2::UNIT_TYPEID::ZERG_HIVE: return true;
+		default: return false;
+		}
+	}
+};
 
 class MacroManager{
 
@@ -17,14 +29,19 @@ public:
 	bool ManageOverlordProduction();
 	bool ManageGeyserProduction();
 	bool ManageZerglingProduction();
+	bool ManageQueenProduction();
+	bool ManageHydraliskProduction();
 
 	bool ManageDrones();
 
-	bool OrderDrones(int quantity);
+	bool OrderDrones();
 	bool OrderOverlords(int quantity);
-	bool OrderZergling(int quantity);
+	bool OrderHydralisk();
+	bool OrderZergling();
+	bool OrderQueen();
 
 	void OnStep();
 
 	sc2::Units GetLarvae();
+	bool GetRandomUnit(const sc2::Unit*& unit_out, const sc2::ObservationInterface* observation, sc2::UnitTypeID unit_type);
 };
